b32ed1021b58c0b5e64f87a346d98904a2c076a2
devopsingh <devopsingh@gmail.com>
2019-05-16 15:15:08 +0100

    updated comment

--- codedeploy/after_install.sh
+++ codedeploy/after_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# This script is executed after the source is copied to the instances
+# This script is executed after the source is copied to the EC2 instances
 cd /usr/cddemo
 npm install
